d6d4e935bddd7c99412d88db680dff45e15d4dd3
Starinin Andrey <starinin-andrey@ya.ru>
2025-05-06 13:45:33 +0300

    Создание консольного приложения

--- PhoneBook_Coll/Program.cs
+++ PhoneBook_Coll/Program.cs
@@ -1,29 +1,75 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using PhoneBook_Coll.Core;
 
-/*var phoneBook = new PhoneBook();
-phoneBook.TryAdd(new Person()
-{
-    FirstName = "Ivan",
-    LastName = "Ivanov",
-    Patronymic = "Ivanovich"
-}, "+79999999999");
-phoneBook.TryAdd(new Person()
-{
-    FirstName = "Ivan",
-    LastName = "Ivanov",
-    Patronymic = "Ivanovich"
-}, "+71231231212");
-phoneBook.TryAdd(new Person()
-{
-    FirstName = "Petr",
-    LastName = "Ivanov",
-    Patronymic = "Ivanovich"
-}, "+71234567890");
+var phoneBook = File.Exists("phonebook.json") 
+    ? PhoneBook.LoadFromFile("phonebook.json") 
+    : new PhoneBook();
 
-PhoneBook.SaveToFile("phonebook.json", phoneBook);*/
-var phoneBook = PhoneBook.LoadFromFile("phonebook.json");
-foreach (var phoneBookItem in phoneBook.GetPhoneBook())
+var actions = new Dictionary<string, Procedure>()
 {
-    Console.WriteLine($"{phoneBookItem.Id}: {phoneBookItem.Person.FullName}, {string.Join(", ", phoneBookItem.PhoneNumbers)}");
+    {"1", () =>
+    {
+        Console.Write("Введите фамилию: ");
+        var lastName = Console.ReadLine();
+        
+        Console.Write("Введите имя: ");
+        var firstName = Console.ReadLine();
+        
+        Console.Write("Введите отчество: ");
+        var patronymic = Console.ReadLine();
+        
+        Console.Write("Введите номер телефона: ");
+        var phoneNumber = Console.ReadLine();
+
+        var person = new Person()
+        {
+            LastName = lastName,
+            FirstName = firstName,
+            Patronymic = patronymic
+        };
+
+        var result = phoneBook.TryAdd(person, phoneNumber);
+
+        Console.WriteLine(result ? "Контакт успешно добавлен" : "Контакт с таким номером телефона уже существует");
+    } },
+    {"2", () =>
+    {
+        var list = phoneBook.GetPhoneBook();
+
+        if (list.Count == 0)
+        {
+            Console.WriteLine("Контакты отсутствуют");
+            return;
+        }
+        
+        foreach (var item in list)
+        {
+            Console.WriteLine($"{item.Id}: {item.Person.FullName} {string.Join(", ", item.PhoneNumbers)}");
+        }
+    } },
+    {"0", () =>
+    {
+        var list = phoneBook.GetPhoneBook();
+        if (list.Count == 0)
+        {
+            Environment.Exit(0);
+        }
+        
+        PhoneBook.SaveToFile("phonebook.json", phoneBook);
+        Environment.Exit(0);
+    }}
+};
+
+while (true)
+{
+    Console.WriteLine("1. Добавить контакт");
+    Console.WriteLine("2. Просмотреть контакты");
+    Console.WriteLine("0. Выход");
+    Console.Write("Введите команду: ");
+    var choice = Console.ReadLine();
+    actions[choice]();
 }
+
+public delegate void Procedure();
