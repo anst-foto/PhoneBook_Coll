0d7e5822efb95a797aa847a28c3db9dfc3242090
Starinin Andrey <starinin-andrey@ya.ru>
2025-05-06 09:52:01 +0300

    Init

---
+++ PhoneBook_Coll/PhoneBook.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace PhoneBook_Coll;
+
+public class PhoneBook
+{
+    private readonly Dictionary<Person, List<string>> _phoneBook;
+
+    public PhoneBook()
+    {
+        _phoneBook = new Dictionary<Person, List<string>>();
+    }
+
+    public bool TryAdd(Person person, string phoneNumber)
+    {
+        foreach (var phones in _phoneBook.Values)
+        {
+            foreach (var phone in phones)
+            {
+                if (phone == phoneNumber) return false;
+            }
+        }
+        
+        if (_phoneBook.ContainsKey(person))
+        {
+            _phoneBook[person].Add(phoneNumber);
+            return true;
+        }
+        
+        _phoneBook.Add(person, new List<string> {phoneNumber});
+        return true;
+    }
+}
---
+++ PhoneBook_Coll/Program.cs
@@ -0,0 +1,3 @@
+﻿using System;
+using PhoneBook_Coll;
+
